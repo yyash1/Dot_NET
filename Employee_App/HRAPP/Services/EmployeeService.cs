3496921a9d63f5b1f3b527edfd5928db716591f2
Yash Dharmik <100307909+yyash1@users.noreply.github.com>
2024-01-04 02:40:32 +0530

    Update EmployeeService.cs

--- Employee_App/HRAPP/Services/EmployeeService.cs
+++ Employee_App/HRAPP/Services/EmployeeService.cs
@@ -5,10 +5,6 @@ namespace HRAPP.Services;
 public class EmployeeService:IEmployeeService{
     public List<Employee> GetAll(){
        List<Employee> employees=new List<Employee>();
-       /*employees.Add(new Employee{Id=1, Name="Shivam"});
-       employees.Add(new Employee{Id=2, Name="Chirag"});
-       employees.Add(new Employee{Id=1, Name="Manisha"});
-       employees.Add(new Employee{Id=1, Name="shailesh"});*/
        RepositoryManager mgr=new RepositoryManager();
        string fileName=@"D:\GITHub\Dot_NET\Employee_App\employees.json";
        employees=mgr.DeSerialize(fileName);
